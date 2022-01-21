6e7441d3debed36d1dcbb0336d0c1cbf86810497
Shehbaz33 <shehbazsayed33@gmail.com>
2022-01-21 12:31:06 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.json({msg:"Home"});
+});
+
+app.get("/hello",(req,res) => {
+    res.json({msg:"Hello"});
+});
+
+app.get("/world",(req,res) => {
+    res.json({msg:"World"});
+});
+
+app.get("/admin",(req,res) => {
+    res.json({msg:"dashboard"});
+});
+
+app.listen(5000,() => console.log('running on port 5000'));
+
