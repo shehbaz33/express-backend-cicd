d1317310fcdff462ad45a24fcece16e91fb2541d
Shehbaz33 <shehbazsayed33@gmail.com>
2022-01-21 12:53:07 +0530

    removed pages

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ app.get("/",(req,res) => {
 });
 
 app.get("/hello",(req,res) => {
-    res.json({msg:"Hello page"});
+    res.json({msg:"Hello"});
 });
 
 app.get("/world",(req,res) => {
-    res.json({msg:"World page"});
+    res.json({msg:"World"});
 });
 
 app.get("/admin",(req,res) => {
-    res.json({msg:"admin page"});
+    res.json({msg:"admin"});
 });
 
 app.listen(5000,() => console.log('running on port 5000'));
